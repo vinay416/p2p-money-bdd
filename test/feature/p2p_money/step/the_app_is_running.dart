1ed94373e4cbbf926494bacbf568df42dadfa1fa
Vinay_Sharma <vinaysharma31.12.1999@gmail.com>
2025-05-20 13:10:05 +0530

    refactors

--- test/feature/p2p_money/step/the_app_is_running.dart
+++ test/feature/p2p_money/step/the_app_is_running.dart
@@ -1,7 +1,7 @@
 import 'package:flutter_test/flutter_test.dart';
 import 'package:fintech_p2p_money/main.dart';
 
-import '../setup_test_di.dart';
+import '../../setup_test_di.dart';
 
 Future<void> theAppIsRunning(WidgetTester tester) async {
   await setupTestDI();
